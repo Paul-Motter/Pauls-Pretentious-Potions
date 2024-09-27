a1c42481a0d51838f2e71a0edcd015905321ba7b
Paul Motter <pmotter@calpoly.edu>
2024-09-26 21:34:25 -0700

    /inventory/audit fix

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -13,9 +13,9 @@ router = APIRouter(
 
 @router.get("/audit")
 def get_inventory():
-    
+
     with db.engine.begin() as connection:
-        inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchall
+        inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchall()
     """ """
     return {"number_of_potions": inventory[0][1], "ml_in_barrels": inventory[0][2], "gold": inventory[0][3]}
 
