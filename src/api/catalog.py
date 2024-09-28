2b935f3259b6e94e05232b7c383e6b3fa5f29433
Paul Motter <pmotter@calpoly.edu>
2024-09-28 10:25:26 -0700

    Version 1 1.1

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -4,19 +4,30 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
+#sample catalogue entry
+#   {
+#       "sku": String ALL_CAPS
+#       "name": String all_lowercase
+#       "quantity": int
+#       "price": int
+#       "potion type": arr[4] e.g. [10,20,30,40]
+#   }
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
-    """
-    Each unique item combination must have only a single price.
-    """
+    """Each unique item combination must have only a single price. max of 6 potion SKUs offered at one time."""
+    catalogueEntries = []
+    with db.engine.begin() as connection:
+        inventory = connection.execute(sqlalchemy.text("SELECT num_green_potions from global_inventory")).fetchall()
 
-    return [
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
-        ]
+    catalogueEntries.append({
+        "sku": "GREEN100",
+        "name": "green_potion",
+        "quantity": inventory[0][0],
+        "price": 75,
+        "potion_type": [0, 100, 0, 0],
+
+    })
+
+    print(f"catalogueEntries: {catalogueEntries}")
+
+    return catalogueEntries
